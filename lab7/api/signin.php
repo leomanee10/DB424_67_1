c8a8f6cdb68824385c105f71c7f874cb0f8da3a1
leomanee10 <66112516@dpu.ac.th>
2024-11-07 12:16:13 +0700

    งาน7จบๆ

--- lab7/api/signin.php
+++ lab7/api/signin.php
@@ -1,37 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab7</title>
+    <style>
+        #error {
+                color: red;
+        }
+    </style>
+</head>
+<body>
+    <h3 id="error"></h3>
+    <form name="signup">
 <?php
 session_start();
-require 'db.php'; 
+require 'db.php';
 
 if (isset($_POST['signin'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $sql = 'SELECT * 
-            FROM users  JOIN student  
-            ON username=studentID
-            WHERE username=?';
-    $stmt = $conn->prepare($sql);//ไม่ไว้ใจรหัสได้ แยกข้อมูลออกจากsql 
-    $stmt->bind_param('s', $username);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    if ($row = $result->fetch_assoc()) {
-    if (password_verify($password, $row['password'])) {
-        $_SESSION['user'] = [
-            'studentID'=>$row['studentID'],
-            'firstName'=>$row['firstName'],
-            'lastName'=>$row['lastName'],
-        ];
-       // $_SESSION['studentID'] = $row['studentID'];
-       // $_SESSION['firstName'] = $row['firstName'];
-        //$_SESSION['lastName'] = $row['lastName'];
-       header('Location: index.php');
-       exit();
+    $username = $_POST ['username'];
+$password = $_POST['password'];
+try {
+$sql = 
+'SELECT * FROM users 
+JOIN student  
+ON username = studentID
+WHERE username =? '; //คำสั่งDatabase
+$stmt = $conn->prepare($sql);
+        $stmt->bind_param('s', $username);
+        $stmt ->execute();
+        $result = $stmt ->get_result();
+        if ($row = $result -> fetch_assoc()){
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['user'] = [
+                'studentID'=>$row['studentID'],
+                'firstName'=>$row['firstName'],
+                'lastName'=>$row['lastName'],
+            ];
+
+            //$_SESSION['studentID'] = $row ['studentID'];
+            //$_SESSION['firstName'] = $row['firstName'];
+            //$_SESSION['lastName'] = $row['lastName'];
+            //header('Location: index.php'); //เชื่อมว่าถ้าถูกต้องให้ย้ายไปที่หน้าไหน
+            //exit(); //เป็นคำสั่งจบเลิก เพราะเปลี่ยนหน้าไปแล้ว
+           http_response_code(200);
+           echo 'Success';
+        }
+
+        else {
+            http_response_code(401);
+            echo 'Password ไม่ถูกต้อง';
+        }
     }
     else {
-       echo 'Password ผิดไปใส่มาใหม่ดิ';
-    } 
+        http_response_code(401);
+        echo 'Username ไม่ถูกต้อง';
     }
-    else {
-      echo 'Username ไม่ถูกต้อง';  
-    }
+}
+catch (Exception) {
+    http_response_code(500);
+    echo 'Sever error.';
+}
+}
+else {
+    http_response_code(401);
+    echo "Unauthorized."
 }
 ?>
